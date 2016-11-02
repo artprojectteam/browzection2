d2b6f255201581f0b1fd4329b1db46404dd17a78
Nobuyuki Kondo <kondo@artprojectteam.jp>
2016-11-02 12:46:20 +0900

    browzectionのロジック作成

--- README.md
+++ README.md
@@ -2,10 +2,92 @@
 
 ※[browzection](https://github.com/artprojectteam/browzection)のバージョンアップ版です
 
-User Agentを利用しアクセスしているブラウザの情報をhtmlクラスに挿入
+User Agentを利用しアクセスしている主要なブラウザの情報をhtmlクラスに挿入
 JavaScriptの変数として利用も可能
 
 @author: Nobuyuki Kondo(art project team)<br>
-@support: IE8+, Chrome, Firefox, Safari, Android 2.x, Android 4+, iOS7+
+@support: IE9+, Chrome, Firefox, Safari, Android 2.x, Android 4+, iOS7+
 
-※IE8以下古いブラウザに対応させる場合は[fallback_method.js](https://github.com/artprojectteam/fallback_method)などを利用してメソッドを対応させる必要があります。
+※IE8以下古いブラウザに対応させる場合は[fallback_method.js](https://github.com/artprojectteam/fallback_method)などを利用してメソッドを対応させる必要があります<br>
+※User Agentは偽装が可能なので取得データが正しいとは限らないので注意
+
+
+## install
+
+
+## Used JavaScript
+
+JavaScript内で呼び出したい場合は下記で呼び出して利用可能。
+
+```
+var BRZ = require('browzection2.js');
+```
+
+※先にbrowzection2.jsを読み込んでおくこと
+
+## 付与されるクラス一覧
+
+該当しない場合は接頭辞に`no-`を付与
+
+### ブラウザ
+
+- .(no-)chrome
+- .(no-)safari
+- .(no-)mobile_safari
+- .(no-)firefox
+- .(no-)msie
+- .(no-)msie6
+- .(no-)msie7
+- .(no-)msie8
+- .(no-)msie9
+- .(no-)msie10
+- .(no-)msie11
+- .(no-)msie_edge
+- .(no-)android_default
+
+### エンジン
+
+- .(no-)webkit
+- .(no-)gecko
+
+
+### タブレット
+
+- .(no-)iPad
+- .(no-)androidTablet
+
+
+### モバイル
+
+- .(no-)iPhone
+- .(no-)iPod
+- .(no-)android
+
+
+### デバイス
+
+- .(no-)tablet
+- .(no-)mobile
+- .(no-)desktop
+
+
+### OS
+
+- .(no-)windows
+- .(no-)osx
+- .(no-)ios
+- .(no-)ios7
+- .(no-)ios8
+- .(no-)ios9
+- .(no-)ios10
+- .(no-)android_2_3
+- .(no-)android_4_0
+- .(no-)android_4_1
+- .(no-)android_4_2
+- .(no-)android_4_3
+- .(no-)android_4_4
+- .(no-)android_5_0
+- .(no-)android_5_1
+- .(no-)android_6_0
+
+
--- dist/browzection2.js
+++ dist/browzection2.js
@@ -1,12 +1,181 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"browzection2.js":[function(require,module,exports){
-module.exports = function (ua) {
+module.exports = function (ua, appV) {
   'use strict';
 
-  void 0;
-  return {
-    aaa: '1',
-    bbb: 'test'
+  var isEdge = isIndexSearch('edge'),
+      isChrome = isIndexSearch('chrome'),
+      isSafari = isIndexSearch('safari'),
+      isMobile = isIndexSearch('mobile'),
+      isAndroid = isIndexSearch('android'),
+      isLikeMacOS = isIndexSearch('like mac os x');
+
+  var no_suffix = 'no-';
+  var $html_class = [];
+
+  /** IE */
+  var ie = {
+    msie: isIndexSearch('msie'),
+    msie6: isAppVersionSearch('msie 6.'),
+    msie7: isAppVersionSearch('msie 7.'),
+    msie8: isAppVersionSearch('msie 8.'),
+    msie9: isAppVersionSearch('msie 9.'),
+    msie10: isAppVersionSearch('msie 10.'),
+    msie11: isIndexSearch('trident/7'),
+    msie_edge: isIndexSearch('applewebkit') && isEdge
   };
-}(window.navigator.userAgent.toLowerCase());
+
+  /** BROWSER */
+  var browser = {
+    chrome: isChrome && isSafari && !isEdge,
+    safari: isSafari && !isEdge && !isChrome && !isMobile,
+    mobile_safari: isSafari && isMobile && !isChrome && !isEdge,
+    firefox: isIndexSearch('firefox'),
+    android_default: isAndroid && !isChrome
+  };
+
+  // merge
+  browser = arrayMerge(browser, ie);
+  setupClasses(browser);
+
+  /** BROWSER ENGINE */
+  var engine = {
+    webkit: isIndexSearch('webkit/'), // Safari, Chrome
+    gecko: isIndexSearch('gecko/') || isIndexSearch('fxios/') // Firefox(FxiOSはiOS版firefox)
+  };
+
+  setupClasses(engine);
+
+  /** TABLET */
+  var tablet = {
+    iPad: isIndexSearch('ipad'),
+    androidTablet: isAndroid && !isMobile || isIndexSearch('a1_07') || isIndexSearch('sc-01c') // A1-07, SC-01C is Tablet, But wrote "mobile"(2013.02 Now)
+  };
+
+  setupClasses(tablet);
+
+  /** MOBILE */
+  var mobile = {
+    iPhone: isIndexSearch('iphone'),
+    iPod: isIndexSearch('ipod'),
+    android: isAndroid && isMobile && !tablet.androidTablet
+  };
+
+  setupClasses(mobile);
+
+  /** DEVICE */
+  var device = {
+    tablet: isBoolean(tablet),
+    mobile: isBoolean(mobile),
+    desktop: true
+  };
+  device.desktop = !device.tablet && !device.mobile;
+
+  setupClasses(device);
+
+  /** OS */
+  var os = {
+    windows: isIndexSearch('windows'),
+    osx: isIndexSearch('intel mac os x'),
+    ios: isIndexSearch('os ') && isLikeMacOS,
+    ios7: isIndexSearch('os 7_') && isLikeMacOS,
+    ios8: isIndexSearch('os 8_') && isLikeMacOS,
+    ios9: isIndexSearch('os 9_') && isLikeMacOS,
+    ios10: isIndexSearch('os 10_') && isLikeMacOS,
+    android_2_3: isIndexSearch('android 2.3'),
+    android_4_0: isIndexSearch('android 4.0'),
+    android_4_1: isIndexSearch('android 4.1'),
+    android_4_2: isIndexSearch('android 4.2'),
+    android_4_3: isIndexSearch('android 4.3'),
+    android_4_4: isIndexSearch('android 4.4'),
+    android_5_0: isIndexSearch('android 5.0'),
+    android_5_1: isIndexSearch('android 5.1'),
+    android_6_0: isIndexSearch('android 6.0')
+  };
+
+  setupClasses(os);
+
+  $html_class.push('js');
+
+  // <html> add class
+  var cls = $html_class.join(' '),
+      html = document.getElementsByTagName('html');
+
+  html[0].className += ' ' + cls;
+
+  /**
+   * 文字列検索(UA)
+   * @param str {string}
+   * @returns {boolean}
+   */
+  function isIndexSearch(str) {
+    return ua.indexOf(str) > 0;
+  }
+
+  /**
+   * 文字列検索(App Version)
+   * @param str
+   * @returns {boolean}
+   */
+  function isAppVersionSearch(str) {
+    return appV.indexOf(str) > 0;
+  }
+
+  /**
+   * Object Merge
+   * @returns {object}
+   */
+  function arrayMerge() {
+    var args = Array.prototype.slice.call(arguments),
+        res = {};
+
+    var _loop = function _loop() {
+      var obj = args[i];
+      Object.keys(obj).forEach(function (r) {
+        if (obj.hasOwnProperty(r)) res[r] = obj[r];
+      });
+    };
+
+    for (var i = 0, iLen = args.length; i < iLen; i++) {
+      _loop();
+    }
+
+    return res;
+  }
+
+  /**
+   * Object is one or more true
+   * @param obj {object}
+   * @returns {boolean}
+   */
+  function isBoolean(obj) {
+    for (var key in obj) {
+      if (obj[key] === true) return true;
+    }
+    return false;
+  }
+
+  /**
+   * html_class push
+   * @param obj
+   */
+  function setupClasses(obj) {
+    Object.keys(obj).forEach(function (keys) {
+      var target = obj[keys],
+          res = target ? keys : no_suffix + keys;
+
+      $html_class.push(res);
+    });
+  }
+
+  return {
+    ua: ua,
+    browser: browser,
+    engine: engine,
+    tablet: tablet,
+    mobile: mobile,
+    device: device,
+    os: os
+  };
+}(window.navigator.userAgent.toLowerCase(), window.navigator.appVersion.toLowerCase());
 
 },{}]},{},["browzection2.js"]);
